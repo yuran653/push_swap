360d3eadbc67753b6bd4d7ce992b0fbd33f5ffde
Jerica Goldstein <jgoldste@si-g1.kzn.21-school.ru>
2022-03-23 17:40:30 +0300

    Mar23

--- sort_insert_bak.c
+++ sort_insert_bak.c
@@ -6,7 +6,7 @@
 /*   By: jgoldste <jgoldste@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 23:01:55 by jgoldste          #+#    #+#             */
-/*   Updated: 2022/03/22 23:42:49 by jgoldste         ###   ########.fr       */
+/*   Updated: 2022/03/23 13:45:13 by jgoldste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -126,3 +126,47 @@ void	sort_insert(t_stack *stack_a, t_stack *stack_b)
 		}
 	}
 }
+
+// void	sort_insert(t_stack *stack_a, t_stack *stack_b)
+// {
+// 	while (stack_b->size)
+// 	{
+// 		while (stack_b->head->index < stack_a->head->index
+// 			|| (stack_b->head->index < stack_a->head->next->index
+// 			&& stack_b->head->index > stack_a->head->index))
+// 			{
+// 				make_push(stack_b, stack_a);
+// 				if (stack_a->head->index > stack_a->head->next->index)
+// 				{
+// 					if (stack_b->head->index < stack_b->head->next->index)
+// 						make_swap_both(stack_a, stack_b);
+// 					else
+// 						make_swap(stack_a);
+// 				}
+// 			}
+// 		if (stack_b->head->index < stack_a->head->index
+// 			&& stack_b->head->index < stack_a->head->prev->index)
+// 			{
+// 			while (stack_b->head->index > stack_a->head->index)
+// 				make_reverse(stack_a);
+// 			make_push(stack_b, stack_a);
+// 			}
+// 		else if (stack_b->head->index > stack_a->head->index)
+// 		{
+// 			while (stack_b->head->index > stack_a->head->index)
+// 				make_rotate(stack_a);
+// 			make_push(stack_b, stack_a);
+// 		}
+// 		else
+// 		{
+// 			if (!check_sort_stack(stack_a))
+// 				ft_printf("\tSTACK_A IS SORTED\n");
+// 			ft_printf("\tSTACK_A SIZE = %d\n", stack_a->size);
+// 			exit(0);
+// 		}
+// 	}
+// 	if (!check_sort_stack(stack_a))
+// 		ft_printf("\tSTACK_A IS SORTED\n");
+// 	ft_printf("\tSTACK_A SIZE = %d\n", stack_a->size);
+// 	exit(0);
+// }
